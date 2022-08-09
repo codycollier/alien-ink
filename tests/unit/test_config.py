e5b677341c01e09a19d0e1c93c019d083f57c16e
Cody Collier <cody@telnet.org>
2022-08-08 23:33:32 -0500

    add skeleton of tests

---
+++ tests/unit/test_config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+from importlib import reload
+import os
+import unittest
+
+from alien_ink import config
+
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_config_defaults(self):
+        reload(config)
+        self.assertEqual(config._config_file_ink, "alien-ink.json")
+
+
+
+if __name__ == "__main__":
+
+    unittest.main()
