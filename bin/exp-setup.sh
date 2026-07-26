7a62a5945e0534ccb792d4aa175067d6128d5abc
Cody Collier <cody@telnet.org>
2026-07-26 13:10:27 -0500

    Add helper

---
+++ bin/exp-setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+#
+#
+#
+
+echo "-------------------------------------------------------------------------"
+echo ":: Alien-Ink Experiments - setup"
+echo "-------------------------------------------------------------------------"
+
+
+# Go to root of repository
+cd $(dirname $0)
+cd ../
+
+# Ensure environment
+echo "-------------------------------------------------------------------------"
+echo ":: Creating and activating virt"
+echo "-------------------------------------------------------------------------"
+uv venv
+source .venv/bin/activate
+
+# Ensure deps and install ink
+echo "-------------------------------------------------------------------------"
+echo ":: Installing alien-ink and depedencies"
+echo "-------------------------------------------------------------------------"
+uv pip install -e ".[hf]"
+
+# Instruction
+echo "-------------------------------------------------------------------------"
+echo ":: ..."
+echo "-------------------------------------------------------------------------"
+echo
+echo "> Environment is ready"
+echo "> Do:"
+echo "$ source .venv/bin/activate"
+echo "$ python -m alien_ink.exp.gpt2_pretrain_wikitext --flight-check"
+echo
+echo
+
