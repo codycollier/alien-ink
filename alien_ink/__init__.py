ff49f018765075afbf86d212f7f8eb6afa326b39
Cody Collier <cody@telnet.org>
2026-07-26 15:42:49 -0500

    Adjust header

--- alien_ink/__init__.py
+++ alien_ink/__init__.py
@@ -2,27 +2,32 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
+
 try:
     __version__ = version("alien-ink")
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
 # Multi-line brand banner (starfield + wordmark). Logged at experiment start.
-HEADER = r"""
+stars = rf"""
+-------------------------------------------------------------------------------
  ..   . *      .     .   * *. .        . .  .     .  *           `         .
 *         . .         ` `  *   . . .            . **              .*    * .  `
      .           . .            ..     *  .               *    .   .
+. ..` .              *.  **    . * *  . . .     *   ` . `  .     .. .  . . ..
+    _    _ _              ___       _
+   / \  | (_) ___ _ __   |_ _|_ __ | | __
+  / _ \ | | |/ _ \ '_ \   | || '_ \| |/ /
+ / ___ \| | |  __/ | | |  | || | | |   <
+/_/   \_\_|_|\___|_| |_| |___|_| |_|_|\_\
 
-     _    _     _              ___       _
-    / \  | |   (_) ___ _ __   |_ _|_ __ | | __
-   / _ \ | |   | |/ _ \ '_ \   | || '_ \| |/ /
-  / ___ \| |___| |  __/ | | |  | || | | |   <
- /_/   \_\_____|_|\___|_| |_| |___|_| |_|_|\_\
-
+*         . .         ` `  *   . . .            . **              .*    * .  `
                     . ..` .              *.  **    . * *  . . .     *   `
 . `  .     .. .  . . ..     .       `.              . *          *   .      .
           *         ..  .  `    `*   `    *   `.   .    `  .. .    . *   ..
+Version: {__version__}
+-------------------------------------------------------------------------------
 """
 
-# Backward-compatible alias for the starfield banner.
-stars = HEADER
+# Alias for the experiment header
+HEADER = stars
